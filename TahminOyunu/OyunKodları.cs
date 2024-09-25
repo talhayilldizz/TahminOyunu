03a44fde68da0964a2cc13d4332c3cf97d4c19e4
Talha <117804171+talhayilldizz@users.noreply.github.com>
2024-09-25 17:48:59 +0300

    Proje dosyası ekle.

---
+++ TahminOyunu/OyunKodları.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TahminOyunu
+{
+    internal class OyunKodları
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("0-10 Arasında Belirlediğimiz Sayıyı Bul..");
+            ArrayList NewList = new ArrayList();
+
+            Random rnd = new Random();
+            int kod = rnd.Next(0, 10);
+            NewList.Add(kod);
+
+            int guess = 3;
+
+            for (int i = 1; i < 4; i++)
+            {
+                Console.WriteLine(i + ". " + "Tahmini Yap.");
+                int value1 = Convert.ToInt32(Console.ReadLine());
+                bool control = NewList.Contains(value1); //Dizide o değer varmı diye bakıyor
+
+                if (control == true)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Tahmin Doğru.. Tebrikler");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Hatalı Tahmin Yaptınız..");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    guess--;
+                    Console.WriteLine("Kalan Hakkınız: " + guess);
+                }
+            }
+            foreach (var a in NewList)
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Sistemin Belirlediği Sayı: " + a);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
